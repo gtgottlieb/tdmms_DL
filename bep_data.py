d04e141c1f6d3ce10e158f466441b083b00294cb
Linuxable <abelloekdelange@gmail.com>
2024-09-10 16:41:27 +0200

    steps to integrate mask loading

--- bep_data.py
+++ bep_data.py
@@ -3,25 +3,18 @@
 import os
 import json
 
+from pycocotools.coco import COCO
 from mrcnn.utils import Dataset
 
 class bepDataset(Dataset):
+    """
+    Class to load the BEP data.
+    """
     class_variable_mapping = {
         'mono': 1,
         'few': 2,
         'thick': 3
     }
-    
-    def load_dir(self, path: str):
-        """"Function to load image directory"""
-        imgs = [i for i in os.listdir(path) if os.path.isfile(os.path.join(path, i))]
-        
-        for i in imgs:
-            self.add_image(
-                path=os.path.join(path, i),
-                source=path,
-                image_id=i
-            )
             
     def convert_annotations_to_coco(self, path: str):
         """
@@ -29,11 +22,12 @@ class bepDataset(Dataset):
         to the classic COCO annotation format.
         
         Args:
-            - path: str, path to .ndjson annotation file
+            - path: str, path to .ndjson annotation file, WITHOUT file extension
+                example: '../../test_annotations'
         Returns:
             - COCO format dictionary
         """
-        with open(path) as f:
+        with open(path+'.ndjson') as f:
             rows = [json.loads(l) for l in f.readlines()]
             
         coco_format = {
@@ -45,7 +39,8 @@ class bepDataset(Dataset):
                 "url": "tudelft.nl", 
                 "contributor": "Abel de Lange"
             }, 
-            "annotations": []
+            "annotations": [],
+            "categories": ['mono', 'few', 'thick']
         }
 
         annotation_id = 1
@@ -73,7 +68,36 @@ class bepDataset(Dataset):
                     }
                     coco_format['annotations'].append(annotation_info)
                     annotation_id += 1
-        return coco_format
+                    
+        with open(path+'.json', 'w+') as f:
+            json.dump(coco_format, f)
+        
+        return None
+    
+    def load_dir(self, path: str, data_type: str, reload_annotations=False):
+        """"Function to load image directory"""
+        img_dir = os.path.join(path, data_type)
+        annotations_file = os.path.join(path, 'annotations', f'{data_type}')
+        
+        if not os.path.isfile(annotations_file+'.json') or reload_annotations:
+            self.convert_annotations_to_coco(annotations_file)
+        
+        annotations_file += '.json'
+        
+        imgs = [i for i in img_dir if os.path.isfile(os.path.join(img_dir, i))]        
+        coco = COCO(annotations_file)
+        
+        class_ids = sorted(coco.getCatIds())
+        
+        print(class_ids)
+
+        
+        for i in imgs:
+            self.add_image(
+                path=os.path.join(path, i),
+                source=path,
+                image_id=i
+            )
         
     def __str__(self):
         return '\n'.join([i['path'] for i in self.image_info])
