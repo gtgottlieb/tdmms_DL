c37a7b77e69c01b29f2b82401e465154812bc64b
Linuxable <abelloekdelange@gmail.com>
2024-09-11 11:58:48 +0200

    load_dataset function finished

--- bep_data.py
+++ bep_data.py
@@ -2,6 +2,7 @@
 
 import os
 import json
+import cv2
 
 from pycocotools.coco import COCO
 from mrcnn.utils import Dataset
@@ -16,18 +17,19 @@ class bepDataset(Dataset):
         'thick': 3
     }
             
-    def convert_annotations_to_coco(self, path: str):
+    def convert_annotations_to_coco(self, annotations_file: str, img_dir: str):
         """
         Function to convert the Labelbox.com .ndjson annotation output 
         to the classic COCO annotation format.
         
         Args:
-            - path: str, path to .ndjson annotation file, WITHOUT file extension
+            - annotations_file: str, path to .ndjson annotation file, WITHOUT file extension
                 example: '../../test_annotations'
+            - img_dir: str, path to image directory
         Returns:
             - COCO format dictionary
         """
-        with open(path+'.ndjson') as f:
+        with open(annotations_file+'.ndjson') as f:
             rows = [json.loads(l) for l in f.readlines()]
             
         coco_format = {
@@ -38,9 +40,14 @@ class bepDataset(Dataset):
                 "date_created": "",
                 "url": "tudelft.nl", 
                 "contributor": "Abel de Lange"
-            }, 
+            },
+            "categories": [
+                {"id": 1, "name": "Mono_Graphene"},
+                {"id": 2, "name": "Few_Graphene"},
+                {"id": 3, "name": "Thick_Graphene"}
+            ],
             "annotations": [],
-            "categories": ['mono', 'few', 'thick']
+            "images": [],
         }
 
         annotation_id = 1
@@ -69,7 +76,25 @@ class bepDataset(Dataset):
                     coco_format['annotations'].append(annotation_info)
                     annotation_id += 1
                     
-        with open(path+'.json', 'w+') as f:
+        imgs = [i for i in os.listdir(img_dir) if os.path.isfile(os.path.join(img_dir, i))]
+        
+        img_sample = cv2.imread(os.path.join(img_dir, imgs[0]))
+        img_height, img_width, _ = img_sample.shape
+        
+        for idx, img in enumerate(imgs):
+            coco_format['images'].append({
+                "id": idx,
+                "path": os.path.join(img_dir, img),
+                "file_name": img,
+                "width": img_width,
+                "height": img_height,
+                "date_captured": "",
+                "license": 1,
+                "coco_url": "",
+                "flickr_url": ""
+            })
+                    
+        with open(annotations_file+'.json', 'w+') as f:
             json.dump(coco_format, f)
         
         return None
@@ -80,24 +105,28 @@ class bepDataset(Dataset):
         annotations_file = os.path.join(path, 'annotations', f'{data_type}')
         
         if not os.path.isfile(annotations_file+'.json') or reload_annotations:
-            self.convert_annotations_to_coco(annotations_file)
-        
-        annotations_file += '.json'
-        
-        imgs = [i for i in img_dir if os.path.isfile(os.path.join(img_dir, i))]        
-        coco = COCO(annotations_file)
+            self.convert_annotations_to_coco(annotations_file, img_dir)
+                        
+        coco = COCO(annotations_file + '.json')
         
         class_ids = sorted(coco.getCatIds())
+        image_ids = list(coco.imgs.keys())
         
-        print(class_ids)
-
-        
-        for i in imgs:
+        for i in class_ids:
+            self.add_class("ali", i, coco.loadCats(i)[0]["name"])
+                
+        for i in image_ids:
             self.add_image(
-                path=os.path.join(path, i),
-                source=path,
-                image_id=i
-            )
+                "ali",
+                image_id=i,
+                path=os.path.join(img_dir, coco.imgs[i]['file_name']),
+                width=coco.imgs[i]["width"],
+                height=coco.imgs[i]["height"],
+                annotations=coco.loadAnns(coco.getAnnIds(
+                    imgIds=[i],
+                    catIds=class_ids,
+                    iscrowd=None
+                )))
         
     def __str__(self):
         return '\n'.join([i['path'] for i in self.image_info])
